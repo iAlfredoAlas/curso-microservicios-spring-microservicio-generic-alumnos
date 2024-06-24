e9b115b7fbf86ab271366dd50d6f8b9b6bc0b0cb
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-24 00:11:44 -0600

    add project files

---
+++ src/main/java/com/curso/microservicios/spring/generic/alumnos/CursoMicroserviciosSpringMicroservicioGenericAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.generic.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroserviciosSpringMicroservicioGenericAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviciosSpringMicroservicioGenericAlumnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/microservicios/spring/generic/alumnos/CursoMicroserviciosSpringMicroservicioGenericAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.generic.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosSpringMicroservicioGenericAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
