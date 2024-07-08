27195a9dfa1042179176a650720c8388d3b46843
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-08 12:29:54 -0600

    delete unused imports

--- src/main/java/com/curso/microservicios/spring/generic/alumnos/models/entity/Alumno.java
+++ src/main/java/com/curso/microservicios/spring/generic/alumnos/models/entity/Alumno.java
@@ -12,8 +12,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import javassist.expr.Instanceof;
-
 @Entity
 @Table(name = "alumnos")
 public class Alumno {
